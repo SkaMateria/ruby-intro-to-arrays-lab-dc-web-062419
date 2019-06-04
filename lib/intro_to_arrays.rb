f8c0fea93454a7d61b5b33b5a186ac3289e7b96d
Carlos Velazquez <skamateria@gmail.com>
2019-06-04 22:55:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -28,3 +28,13 @@ def first_element_with_array_methods(first_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   first_country = @south_east_asia.first
 end
+
+def last_element_with_array_methods(last_country)
+  @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  last_country = @south_east_asia.last_country
+end
+
+
+
+
+
